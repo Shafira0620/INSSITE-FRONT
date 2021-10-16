e23545c9caa4195fddf299442c1281a817c1ff45
fernandoCV19 <64548505+fernandoCV19@users.noreply.github.com>
2021-10-16 17:40:36 -0400

    Correcion en la creacion del modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainViewComponent } from './Student/main-view/main-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainViewComponent
   ],
   imports: [
     BrowserModule,
