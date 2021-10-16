a42f1ae9b6a882076cf22a0e5abf54a47871c331
fernandoCV19 <64548505+fernandoCV19@users.noreply.github.com>
2021-10-16 17:58:25 -0400

    Anadido componentes material de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeViewStudentComponent } from './student/welcome-view-student/welcome-view-student.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { WelcomeViewStudentComponent } from './student/welcome-view-student/welc
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
