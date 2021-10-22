b00fb7b51bdfa51fe5783b559bf39589ca7a0ede
Shafira0620 <hinojosanasha@gmail.com>
2021-10-22 18:09:25 -0400

    Login terminado

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/inssite-front'),
+      dir: require('path').join(__dirname, './coverage/Inssite1'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'inssite-front'`, () => {
+  it(`should have as title 'Inssite1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('inssite-front');
+    expect(app.title).toEqual('Inssite1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('inssite-front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Inssite1 app is running!');
   });
 });
