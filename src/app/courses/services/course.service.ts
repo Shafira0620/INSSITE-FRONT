8ce62b1a3e35a8b2255ddcad1b0f8c4e79f9d50d
achigy <gonzalesalison775@gmail.com>
2021-10-22 15:28:11 -0400

    Anadir la ruta al idUser

--- src/app/courses/services/course.service.ts
+++ src/app/courses/services/course.service.ts
@@ -6,11 +6,12 @@ import { Observable } from 'rxjs/internal/Observable';
   providedIn: 'root'
 })
 export class CourseService {
-  BASE_URL:string='http://localhost:3000'
+  BASE_URL:string='http://localhost:3000/'
   constructor(private http:HttpClient) { }
   getCourses():Observable<Course[]>{
-    return this.http.get<Course[]>('$(this.BASE_URL)/courses');
+    return this.http.get<Course[]>('$(this.BASE_URL)/course/:idUser');
   }
+
   getCourse(id: number):Observable<Course>{
     return this.http.get<Course>('$(this.BASE_URL)/courses/${id}');
   }
