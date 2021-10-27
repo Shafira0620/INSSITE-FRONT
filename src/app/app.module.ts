afb5756be9486cb9192adec25e2440a7da15a04a
Shafira0620 <hinojosanasha@gmail.com>
2021-10-27 01:23:04 -0400

    Funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'; 
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component'
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { LoginService} from '../services/login.service'
+import { LoginI } from '../models/login.interface'
+import { ResponseI } from '../models/response.interface'
+
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  })
 
-  constructor() { }
+  constructor(private http:LoginService, private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  onLogin(form:LoginI){
+    //console.log(form);
+    this.http.Login(form).subscribe(data => {
+      let response:ResponseI = data
+      if (response) {  //validar status
+        localStorage.setItem('token', response.data.accessToken)
+        //console.log(response.data.accessToken);
+        this.router.navigate(['/home'])
+        
+      }
+
+    })
   }
 
 }
